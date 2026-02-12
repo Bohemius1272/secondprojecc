217c70bb11e6a6618b4720da0eefd8813f883032
Bohemius1272 <JerryKnyazhevsky@gmail.com>
2026-02-12 11:48:23 -0500

    third

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -15,3 +15,23 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
    return Text("Questions Screen");
   }
 }
+
+class _QuestionScreenState extends State<QuestionsScreen>{
+  @override
+  Widget build(BuildContext context) {
+
+   return SizedBox(
+    width: double.infinity,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      const Text('THIS IS THE QUESTION'),
+      const SizedBox(height:30),
+      ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+      ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
+      ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+      ElevatedButton(onPressed: () {}, child: const Text('Answer 4')),
+    ],),
+   );
+  }
+}
