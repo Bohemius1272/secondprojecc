4d589aa939458b96686642c1896aa5d30ca2dfdd
Bohemius1272 <JerryKnyazhevsky@gmail.com>
2026-02-12 11:34:34 -0500

    first commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,31 @@
+import 'package:secondprojecc/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Can your ideas change the world?'
+    , [
+      'Yes',
+      'No',
+      'Maybe',
+      'FLINT LOCKWOOD!!!',
+    ]
+  ),
+  QuizQuestion(
+    'Who lives in a pineapple under the sea?'
+    , [
+      'Spongebob Squarepants',
+      'Jeffrey Epstein',
+      'Invader Zim',
+      'My Uncle Simon',
+    ]
+  ),
+  QuizQuestion(
+    'Who currently has no job right now?'
+    , [
+      'The creator of this quiz',
+      'Deadpool',
+      'A Reddit Moderator',
+      'Asmongold',
+    ]
+  ),
+];
